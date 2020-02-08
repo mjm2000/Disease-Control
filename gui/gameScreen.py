66931e4ead61d1c8a42eedec300aec7c33042319
Squidmaan <longmatt321@gmail.com>
2020-02-08 18:06:35 -0500

    Condensed gui class

--- gui/gameScreen.py
+++ gui/gameScreen.py
@@ -1,6 +1,7 @@
 
 import pygame
 import pygame_gui
+import gui.sideBar
 from pygame.locals import *
 
 
@@ -42,7 +43,6 @@ def run_game():
             if yesButtonPosition.collidepoint(mouse_pos):
                 print("yes")
 
-        #TODO Highlight Button on hover
         #TODO Clickable Button changes button when clicked
 
     def noButton(x,y):
@@ -61,47 +61,10 @@ def run_game():
             mouse_pos = event.pos # Now it will have the coordinates of click point.
             if noButtonPosition.collidepoint(mouse_pos):
                 print("no")
-        #TODO Highlight Button on hover
         #TODO Clickable Button changes button when clicked
 
     #these are the texts for the sidebar
     myfont = pygame.font.SysFont('Comic Sans MS', 20)
-    textsurface = myfont.render('Total Population', False, (255, 255, 255))
-
-    def weektitle(x, y):
-        textsurface = myfont.render('Week:', False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
-
-    def weekvariable(x, y, text):
-        textsurface = myfont.render(text, False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
-
-    def totalpoptitle(x, y):
-        textsurface = myfont.render('Total Population', False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
-
-    def totalpopvariable(x, y, text):
-        # this is only going to do strings so maybe think about a converter so
-        # it can change int to string and back and forth
-        textsurface = myfont.render(text, False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
-
-    def diseasedtitle(x, y):
-        textsurface = myfont.render("Total Diseased", False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
-
-    def diseasedvariable(x, y, text):
-        textsurface = myfont.render(text, False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
-
-    def moraletitle(x, y):
-        textsurface = myfont.render("Morale", False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
-
-    def moralevariable(x, y, text):
-        # this is only going to do strings so maybe think about a converter so it can change int to string and back and forth
-        textsurface = myfont.render(text, False, (255, 255, 255))
-        ourDisplay.blit(textsurface, (x, y))
 
     is_running = True
     while is_running:
@@ -113,19 +76,19 @@ def run_game():
         yesButton(0,400)
         black = (0,0,0)
         pygame.draw.rect(ourDisplay,black,(600,0,700,600))
-        weektitle(660, 70)
-        weekvariable(730, 70, "1")
+        gui.sideBar.weektitle(660,70)
+        gui.sideBar.weekvariable(730, 70, "1")
 
         # should be 30 away from its corresponding variable
         # the 3rd argument is the variable
-        totalpoptitle(625, 180)
-        totalpopvariable(700, 210, "%")
+        gui.sideBar.totalpoptitle(625, 180)
+        gui.sideBar.totalpopvariable(700, 210, "%")
 
-        diseasedtitle(625, 300)
-        diseasedvariable(700, 330, "%")
+        gui.sideBar.diseasedtitle(625, 300)
+        gui.sideBar.diseasedvariable(700, 330, "%")
 
-        moraletitle(660, 420)
-        moralevariable(700, 450, "%")
+        gui.sideBar.moraletitle(660, 420)
+        gui.sideBar.moralevariable(700, 450, "%")
         noButton(300,400)
         pygame.display.update()
 
