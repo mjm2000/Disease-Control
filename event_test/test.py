422f38281eb4969360cf2f684804f3eec3d3158d
Jimmy <cyncatron@gmail.com>
2020-02-08 11:29:18 -0500

    create files

---
+++ event_test/test.py
@@ -0,0 +1,43 @@
+
+import pygame 
+import pygame_gui
+
+
+def run_game():
+    pygame.init() 
+    pygame.display.set_caption('Hello Gamers')
+    win_sur = pygame.display.set_mode((800,600))
+    
+    br = pygame.Surface((800,600))
+    br.fill(pygame.Color('#000f00'))
+
+    manager = pygame_gui.UIManager((800, 600))
+
+    clock = pygame_gui.time.Clock()
+
+    hello_button = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(
+            (350, 275),
+            (100, 50)
+        ),
+        text='Say Hello',
+        manager=manager
+    )
+
+    is_running = True 
+
+    while is_running: 
+        time_delta = clock.tick(60)/1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+            manager.process_events(event)
+
+        win_sur.blit(br,(0,0))
+
+        manager.draw_ui(win_sur)
+
+        pygame.display.update()
+
+
+run_game()
