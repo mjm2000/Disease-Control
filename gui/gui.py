875b5bc428f2ea510dbbeaea3fcc43a1f9e723c3
Squidmaan <longmatt321@gmail.com>
2020-02-08 16:46:09 -0500

    Sidebar added and better buttons

--- gui/gui.py
+++ gui/gui.py
@@ -17,11 +17,12 @@ def run_game():
     manager = pygame_gui.UIManager((800, 600))
 
     #Load Images
-    yesButtonImg = pygame.image.load('yes3.png')
-    noButtonImg = pygame.image.load('nooo.png')
+    yesButtonImg = pygame.image.load('greenButtonwoop.png')
+    noButtonImg = pygame.image.load('redButton.png')
 
     #These two methods create a yes and a no button
     def yesButton(x,y):
+
         yesButtonPosition = Rect(0,x,0,y)
         yesButtonPosition.collidepoint(pygame.mouse.get_pos())
         yesButtonPosition = yesButtonImg.get_rect()
@@ -31,12 +32,22 @@ def run_game():
         if event.type == MOUSEBUTTONDOWN:
             mouse_pos = event.pos # Now it will have the coordinates of click point.
             if yesButtonPosition.collidepoint(mouse_pos):
-                print('LETS GO GAMERS')
+                print("yes")
+
         #TODO Highlight Button on hover
         #TODO Clickable Button changes button when clicked
 
     def noButton(x,y):
+        noButtonPosition = Rect(0,x,0,y)
+        noButtonPosition.collidepoint(pygame.mouse.get_pos())
+        noButtonPosition = noButtonImg.get_rect()
+        noButtonPosition = noButtonPosition.move(x,y)
+
         ourDisplay.blit(noButtonImg, (x, y))
+        if event.type == MOUSEBUTTONDOWN:
+            mouse_pos = event.pos # Now it will have the coordinates of click point.
+            if noButtonPosition.collidepoint(mouse_pos):
+                print("no")
         #TODO Highlight Button on hover
         #TODO Clickable Button changes button when clicked
 
@@ -44,9 +55,40 @@ def run_game():
     myfont = pygame.font.SysFont('Comic Sans MS', 20)
     textsurface = myfont.render('Total Population', False, (255, 255, 255))
 
-    def sideBar(x,y):
-        textSurface = myfont.render('Total Population', False, (255, 255, 255))
-        ourDisplay.blit(textsurface,(x,y))
+    def weektitle(x, y):
+        textsurface = myfont.render('Week:', False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
+
+    def weekvariable(x, y, text):
+        textsurface = myfont.render(text, False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
+
+    def totalpoptitle(x, y):
+        textsurface = myfont.render('Total Population', False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
+
+    def totalpopvariable(x, y, text):
+        # this is only going to do strings so maybe think about a converter so
+        # it can change int to string and back and forth
+        textsurface = myfont.render(text, False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
+
+    def diseasedtitle(x, y):
+        textsurface = myfont.render("Total Diseased", False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
+
+    def diseasedvariable(x, y, text):
+        textsurface = myfont.render(text, False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
+
+    def moraletitle(x, y):
+        textsurface = myfont.render("Morale", False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
+
+    def moralevariable(x, y, text):
+        # this is only going to do strings so maybe think about a converter so it can change int to string and back and forth
+        textsurface = myfont.render(text, False, (255, 255, 255))
+        ourDisplay.blit(textsurface, (x, y))
 
     is_running = True
     while is_running:
@@ -58,7 +100,19 @@ def run_game():
         yesButton(0,400)
         black = (0,0,0)
         pygame.draw.rect(ourDisplay,black,(600,0,700,600))
-        sideBar(625,0)
+        weektitle(660, 70)
+        weekvariable(730, 70, "1")
+
+        # should be 30 away from its corresponding variable
+        # the 3rd argument is the variable
+        totalpoptitle(625, 180)
+        totalpopvariable(700, 210, "%")
+
+        diseasedtitle(625, 300)
+        diseasedvariable(700, 330, "%")
+
+        moraletitle(660, 420)
+        moralevariable(700, 450, "%")
         noButton(300,400)
         pygame.display.update()
 
