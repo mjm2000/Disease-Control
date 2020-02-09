d0b2ee348c63f28a2cde8940a3260dcbed10088c
ajbrusic <ajbrusic@buffalo.edu>
2020-02-09 04:45:55 -0500

    change question

--- event_test/results.py
+++ event_test/results.py
@@ -1,9 +1,34 @@
+import random
 week = 1
 morale = 60
 population = 100
 diseased = 5
 
-tier = 1
+question = 1.1
+
+def change_question(yes_answer):
+    global question
+
+    if question < 4:
+        if question == 1:
+            question = 2
+        elif question == 2:
+            if yes_answer:
+                question = 3.1
+            else:
+                question = 3.2
+        elif 3 <= question < 4:
+            if yes_answer:
+                question += 1
+            else:
+                question += 0.9
+    else:
+        # make random number
+        rand = random.randint(0,3)
+        rand /= 10
+        question = round(question) + rand + 1
+
+
 
 def get_results(pop, mor, dis):
     global week
@@ -18,12 +43,8 @@ def get_results(pop, mor, dis):
     global diseased
     diseased += dis
 
-    #update tiers if necessary
-    if morale > 80:
-        tier = 2
+question = 2.2
 
-    elif morale < 40:
-        tier = 0
+change_question(True)
 
-    else:
-        tier = 1
+print(question)
